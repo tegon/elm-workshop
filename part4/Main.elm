fbb7627ee899c1ee05c0390ea24be5ff8d6da38b
Leonardo Tegon <leonardo.tegon@plataformatec.com.br>
2017-11-30 19:12:47 -0200

    Complete part4

--- part4/Main.elm
+++ part4/Main.elm
@@ -24,11 +24,7 @@ type alias Msg =
     }
 
 
-{-| TODO add a type annotation to this value
-
-HINT: The type aliases above may come in handy for these exercises!
-
--}
+initialModel : Model
 initialModel =
     { query = "tutorial"
     , results =
@@ -56,8 +52,7 @@ initialModel =
     }
 
 
-{-| TODO add a type annotation to this function
--}
+view : Model -> Html Msg
 view model =
     let
         elmHubHeader =
@@ -72,8 +67,7 @@ view model =
         ]
 
 
-{-| TODO add a type annotation to this function
--}
+viewSearchResult : SearchResult -> Html Msg
 viewSearchResult result =
     li []
         [ span [ class "star-count" ] [ text (toString result.stars) ]
@@ -85,8 +79,7 @@ viewSearchResult result =
         ]
 
 
-{-| TODO add a type annotation to this function
--}
+update : Msg -> Model -> Model
 update msg model =
     if msg.operation == "DELETE_BY_ID" then
         { model
